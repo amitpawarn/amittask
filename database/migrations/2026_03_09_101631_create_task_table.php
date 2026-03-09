230213a3e0598c3ff086f672ef3df102c55636c2
amitpawarn <amitp.nethority@gmail.com>
2026-03-09 16:32:40 +0530

    first commit

---
+++ database/migrations/2026_03_09_101631_create_task_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('task', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users')->cascadeOnDelete();
+            $table->string('task_name');
+            $table->foreignId('project_id')->constrained('projects')->cascadeOnDelete();
+            $table->string('project_name');
+            $table->enum('status',['pending','on-going','testing','done'])->default('pending');
+            $table->date('start_date');
+            $table->date('due_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('task');
+    }
+};
